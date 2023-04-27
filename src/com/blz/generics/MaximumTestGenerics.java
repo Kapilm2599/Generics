34bf60b72b411ed91d69f41fc6e464f46be8ba53
Kapilm2599 <mahajankapil575@gmail.com>
2023-04-27 11:40:23 +0530

    Refactor: To create generics class to take 3 variables of generic type

--- src/com/blz/generics/MaximumTestGenerics.java
+++ src/com/blz/generics/MaximumTestGenerics.java
@@ -3,26 +3,34 @@ package com.blz.generics;
 public class MaximumTestGenerics<T>{
 	T num1, num2, num3;
 	
+	public MaximumTestGenerics(T num1, T num2, T num3) {
+		this.num1 = num1;
+		this.num2 = num2;
+		this.num3 = num3;
+		
+	}
+	
 	//public static Integer maximumInteger(Integer num1, Integer num2, Integer num3)
 	//public static Float maximumFloat(Float num1, Float num2, Float num3)
 	//public static String maximumString(String num1, String num2, String num3) 
-	public static <T extends Comparable<T>> T maximumAmongThree(T num1, T num2, T num3) {
+	public static <T extends Comparable<T>> T MaximumAmongThree(T num1, T num2, T num3) {
 
-		T maximumNumber = num1;
+		T maximum = num1;
 		if (num1.compareTo(num2) > 0 && num1.compareTo(num3) > 0)
-			maximumNumber = num1;
+			maximum = num1;
 		else if (num2.compareTo(num1) > 0 && num2.compareTo(num3) > 0)
-			maximumNumber = num2;
+			maximum = num2;
 		else
-			maximumNumber = num3;
-		return maximumNumber;
+			maximum = num3;
+		return maximum;
+
 	}
 
 	public static void main(String[] args) {
-		System.out.println(
-				"The max Number Among Three String numbers is:-\n " + maximumAmongThree("Papaya", "Apple", "Mango"));
-		System.out.println("The max Number Among Three Integer numbers is:-\n " + maximumAmongThree(70, 40, 50));
-		System.out.println("The max Number Among Three Float numbers is:-\n " + maximumAmongThree(70.6f, 40.7f, 50.8f));
+		System.out.println("The max Number Among Three String numbers is:-\n "
+				+ MaximumAmongThree("Orange", "Dragonfruit", "Apple"));
+		System.out.println("The max Number Among Three Integer numbers is:-\n " + MaximumAmongThree(14, 56, 27));
+		System.out.println("The max Number Among Three Float numbers is:-\n " + MaximumAmongThree(39.6f, 78.7f, 99.8f));
 	}
 
 }
