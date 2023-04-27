d7d68847c4891ceed1ec2b8c4135d06674209719
Kapilm2599 <mahajankapil575@gmail.com>
2023-04-27 11:21:25 +0530

    Added: To getting the maximum string

--- src/com/blz/generics/MaximumTestGenerics.java
+++ src/com/blz/generics/MaximumTestGenerics.java
@@ -3,21 +3,23 @@ package com.blz.generics;
 public class MaximumTestGenerics {
 	
 	//public static Integer maximumInteger(Integer num1, Integer num2, Integer num3)
-	public static Float maximumFloat(Float num1, Float num2, Float num3) {
+	//public static Float maximumFloat(Float num1, Float num2, Float num3)
+	public static String maximumString(String num1, String num2, String num3) {
 
-		//Integer maximumNumber = num1;
-		Float maximumNumber = num1;
+		String maximumString = num1;
 		if (num1.compareTo(num2) > 0 && num1.compareTo(num3) > 0)
-			maximumNumber = num1;
+			maximumString = num1;
 		else if (num2.compareTo(num1) > 0 && num2.compareTo(num3) > 0)
-			maximumNumber = num2;
+			maximumString = num2;
 		else
-			maximumNumber = num3;
-		return maximumNumber;
+			maximumString = num3;
+		return maximumString;
+
 	}
 
 	public static void main(String[] args) {
-		System.out.println("Maximum Float Number among 3 will be: " + maximumFloat(70.6f, 40.7f, 50.8f));
+		System.out.println("Maximum String Among 3 will be :- " + maximumString("Papaya", "Apple", "Mango"));
 	}
+
 
 }
