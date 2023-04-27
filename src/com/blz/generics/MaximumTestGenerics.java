710219441c2e3bdaec3b19ca676775dea296e879
Kapilm2599 <mahajankapil575@gmail.com>
2023-04-27 11:56:02 +0530

    Added: Extend method of generic

--- src/com/blz/generics/MaximumTestGenerics.java
+++ src/com/blz/generics/MaximumTestGenerics.java
@@ -14,7 +14,7 @@ public class MaximumTestGenerics<T>{
 	//public static Integer maximumInteger(Integer num1, Integer num2, Integer num3)
 	//public static Float maximumFloat(Float num1, Float num2, Float num3)
 	//public static String maximumString(String num1, String num2, String num3) 
-	public static <T extends Comparable<T>> T MaximumAmongFour(T num1, T num2, T num3, T num4) {
+	public static <T extends Comparable<T>> T maximumAmongFour(T num1, T num2, T num3, T num4) {
 
 		T maximum = num1;
 		if (num1.compareTo(num2) > 0 && num1.compareTo(num3) > 0)
@@ -28,13 +28,14 @@ public class MaximumTestGenerics<T>{
 		return maximum;
 
 	}
-
+	
+	public static <E> void printMaximum(E num1, E num2, E num3, E num4, E max) {
+		System.out.println(max);
+	}
 	public static void main(String[] args) {
-		System.out.println("The max Number Among Four String numbers is:-\n "
-				+ MaximumAmongFour("Orange", "Dragonfruit", "Apple", "Mango"));
-		System.out.println("The max Number Among Four Integer numbers is:-\n " + MaximumAmongFour(14, 56, 27, 40));
-		System.out.println(
-				"The max Number Among Four Float numbers is:-\n " + MaximumAmongFour(39.6f, 78.7f, 99.8f, 53.1f));
+		System.out.println("The max Number Among Four String numbers is:-\n "+maximumAmongFour("Orange", "Dragonfruit", "Apple", "Mango"));
+		System.out.println("The max Number Among Four Integer numbers is:-\n "+maximumAmongFour(14, 56, 27, 40));
+		System.out.println("The max Number Among Four Float numbers is:-\n "+maximumAmongFour(39.6f, 78.7f, 99.8f, 53.1f));
 	}
 
 
