a0687f48e53a4da09d856e3b542d3a28426f2cb0
Kapilm2599 <mahajankapil575@gmail.com>
2023-04-27 11:14:44 +0530

    Added: To getting the maximum floats numbers

--- src/com/blz/generics/MaximumTestGenerics.java
+++ src/com/blz/generics/MaximumTestGenerics.java
@@ -2,9 +2,11 @@ package com.blz.generics;
 
 public class MaximumTestGenerics {
 	
-	public static Integer maximumInteger(Integer num1, Integer num2, Integer num3) {
+	//public static Integer maximumInteger(Integer num1, Integer num2, Integer num3)
+	public static Float maximumFloat(Float num1, Float num2, Float num3) {
 
-		Integer maximumNumber = num1;
+		//Integer maximumNumber = num1;
+		Float maximumNumber = num1;
 		if (num1.compareTo(num2) > 0 && num1.compareTo(num3) > 0)
 			maximumNumber = num1;
 		else if (num2.compareTo(num1) > 0 && num2.compareTo(num3) > 0)
@@ -15,7 +17,7 @@ public class MaximumTestGenerics {
 	}
 
 	public static void main(String[] args) {
-		System.out.println("Maximum Number among 3 will be: " + maximumInteger(70, 40, 50));
+		System.out.println("Maximum Float Number among 3 will be: " + maximumFloat(70.6f, 40.7f, 50.8f));
 	}
 
 }
